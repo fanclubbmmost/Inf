c18a1244d5fef5867851568c60d6d2f579d4bc66
Cecr2504a <Oss106a@gmail.com>
2019-03-25 20:35:40 +0100

    Update cz1.py

--- cz1.py
+++ cz1.py
@@ -1,8 +1,34 @@
 import matplotlib.pyplot as plt
-a=[1,2]
-b=[3,5]
-c=[1,1]
-d=[5,1]
+
+a=[]
+b=[]
+c=[]
+d=[]
+print("podaj wspl x punktu a")
+ax=int(input())
+a.append(ax)
+print("podaj wspl y punktu a")
+ay=int(input())
+a.append(ay)
+print("podaj wspl x punktu b")
+bx=int(input())
+b.append(bx)
+print("podaj wspl y punktu b")
+by=int(input())
+b.append(by)
+print("podaj wspl x punktu c")
+cx=int(input())
+c.append(cx)
+print("podaj wspl y punktu c")
+cy=int(input())
+c.append(cy)
+print("podaj wspl x punktu d")
+dx=int(input())
+d.append(dx)
+print("podaj wspl y punktu d")
+dy=int(input())
+d.append(dy)
+
 if (b[0]-a[0])*(d[1]-c[1])-(b[1]-a[1])*(d[0]-c[0])==0:
     print("proste równoległe")
 else:
@@ -34,6 +60,12 @@ else:
             plt.plot(yb, xb, linestyle='--')
             plt.plot(yb, xb, marker='o')
             plt.plot(Yp, Xp, marker='o',color='r')
+            plt.savefig('Wykres.png')
+            DaneABCD=open('daneabcd.txt','w')
+            DaneABCD.write("\n   |{:^20}|{:^20}|{:^20}|{:^20}|{:^20}|".format("A","B","C","D","P"))
+            DaneABCD.write("\n X |{:^20}|{:^20}|{:^20}|{:^20}|{:^20}|".format(a[0],b[0],c[0],d[0],Xp))
+            DaneABCD.write("\n Y |{:^20}|{:^20}|{:^20}|{:^20}|{:^20}|".format(a[1],b[1],c[1],d[1],Yp))
+            DaneABCD.close()
         else:
             print("proste nie przecinają się")      
             x=[a[0],b[0]]
@@ -46,3 +78,10 @@ else:
             plt.xlabel('wspolrzedna y')
             plt.plot(y, x, marker='o')
             plt.plot(yb, xb, marker='o')
+            plt.savefig('Wykres.png')
+            DaneABCD=open('daneabcd.txt','w')
+            DaneABCD.write("\n|{:^20}|{:^20}|{:^20}|{:^20}|".format("A","B","C","D"))
+            DaneABCD.write("\n X |{:^20}|{:^20}|{:^20}|{:^20}|".format(a[0],b[0],c[0],d[0]))
+            DaneABCD.write("\n Y |{:^20}|{:^20}|{:^20}|{:^20}|".format(a[1],b[1],c[1],d[1]))
+            DaneABCD.close()
+
